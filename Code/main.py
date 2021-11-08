472da1897825e0e4b43b635424e293b0881ee9f7
aymaneelasslouj <elassloujaymane@gmail.com>
2021-11-08 03:23:26 -0700

    Version 0.0, partially handles path-finding with a GUI

--- Code/main.py
+++ Code/main.py
@@ -1,6 +1,7 @@
 import Mission_profile_construction as M_p_c
 import Dashboard_GUI as D_g
 
+
 """Create mission profile from json file and create dashboard"""
 Mission_profile = M_p_c.Json_to_mission_profile(file='data.json')
 Mission_profile.Compute_path()
