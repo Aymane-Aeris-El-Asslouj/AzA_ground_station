7b1c2f21e9786f9181e254ed25342f2fc3d60221
aymaneelasslouj <elassloujaymane@gmail.com>
2021-11-08 11:26:49 -0700

    Version 0.0.4, refined file structure and started working on ServerComs

--- avionics_code/communications/server_communications.py
+++ avionics_code/communications/server_communications.py
@@ -1,5 +1,17 @@
-
+from auvsi_suas.client import client
+from auvsi_suas.proto import interop_api_pb2
 
 class ServerComs:
     def __init__(self):
+        pass
+
+    def connect(self):
+        client = client.Client(url='http://127.0.0.1:8000',
+                               username='testuser',
+                               password='testpass')
+
+    def get_mission(self):
+        pass
+
+    def upload_telemetry(self):
         pass
--- avionics_code/main.py
+++ avionics_code/main.py
@@ -11,15 +11,15 @@ def launch_avionics():
     mission profile, then keeps uploading telemetry. In the future,
     it must also upload deliverables of the Computer vision team
     and download other planes' telemetry"""
-    SC = s_c.ServerComs()
+    sc = s_c.ServerComs()
 
     """Connect to the interop server"""
-    SC.connect()
+    sc.connect()
 
     """This Mission profile object stores all the mission info
     provided by the competition's server (border, waypoint,
     obstacles, etc)."""
-    MP = SC.get_mission()
+    mp = sc.get_mission()
 
     """This RF communications object handles communication with
     the plane's Pixhawk and ground vehicle. It downloads plane
@@ -28,14 +28,14 @@ def launch_avionics():
     to call for the server comms to upload telemetry and the 
     mission controller to refresh the mission state or land
     if telemetry is not received."""
-    RF = rf_c.RFComs()
+    rf = rf_c.RFComs()
 
     """Connect to the Pixhawk"""
-    RF.Connect()
+    rf.Connect()
 
     """Download once the info of the plane to create an initial
     flight plan"""
-    RF.fetch_plane_status()
+    rf.fetch_plane_status()
 
     """Mission state object storing the waypoints that the plane needs
     to go through in order to accomplish the entire mission (mission
@@ -43,34 +43,34 @@ def launch_avionics():
     function to generate these true mission waypoints along with
     landing functions in case of timeout or emergency that change
     the plan to landing"""
-    MS = m_s.MissionState()
+    ms = m_s.MissionState()
 
     """Generate complete set of waypoints to accomplish all parts
     of the mission (mission waypoints, UGV drop, map scouting, etc)
     based on the mission profile and the current status of the plane"""
-    MS.generate()
+    ms.generate()
 
     """Mission control object that handles refreshing of mission state
     based on new plane status info, path recomputation/export and its
     conditions, along with timeout landing"""
-    MC = m_c.MissionControl()
+    mc = m_c.MissionControl()
 
     """Compute selected path object that stores the latest exported trajectory
         waypoint list as a Path object computed from the Mission profile,
         Mission State, and current plane status info. Also calls RF comms
         to convert the path into flight plan and export it to the Pixhawk"""
-    MC.computepath()
+    mc.computepath()
 
     """Starts the loop of RF comms that keeps fetching for plane
     status info and calling server comms' telemetry upload and
     mission control's mission state refresh"""
-    RF.loop_fetch_plane_status()
+    rf.loop_fetch_plane_status()
 
     """Graphical User Interface object that allows to visualize
     the progress of the mission along with adding info to the
     mission profile for testing purposes, or issuing drop authorizations
     and emergency landing orders for real missions"""
-    GUI = g_u_i.GUI()
+    gui = g_u_i.GUI()
 
 
 if __name__ == '__main__':
